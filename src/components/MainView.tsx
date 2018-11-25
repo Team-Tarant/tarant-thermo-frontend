1ffa41a574e8222b4973a756ec887d5c1d248bb2
xbexbex <petja.valkama@gmail.com>
2018-11-25 12:16:59 +0200

    added demo reset button

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../styles/MainView.scss';
 import { Button, Drawer } from '@material-ui/core';
 import Room from './Room';
+import axios from 'axios';
+import { serverUrl } from '../utils/env'
 
 class MainView extends Component {
 
@@ -10,16 +12,30 @@ class MainView extends Component {
         console.log(this.props.rooms)
     }
 
+    resetDemo = () => {
+        try {
+            axios.get(serverUrl + '/api/rooms/reset');
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     render() {
         return (
-            <div className="room-container">
-                {Object.keys(this.props.rooms).map((item, i) => {
-                    return <Room
-                        key={i}
-                        data={this.props.rooms[item]}
-                        outsideTemperature={4.3}
-                    />;
-                })}
+            <div>
+                <div className="room-container">
+                    {Object.keys(this.props.rooms).map((item, i) => {
+                        return <Room
+                            key={i}
+                            data={this.props.rooms[item]}
+                            outsideTemperature={4.3}
+                        />;
+                    })}
+                </div>
+                <Button
+                    className="reset-button"
+                    onClick={this.resetDemo}>
+                    Reset</Button>
             </div>
         )
     }
